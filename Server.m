e5c20c25a0fc4dc86f06ea80c40c83ea6edf899d
Deckels <daniel.c.eck@gmail.com>
2017-04-24 17:57:42 -0400

    Add files via upload

--- Server.m
+++ Server.m
@@ -1,20 +1,20 @@
 %% MFCC
 % Voicebox obtained from: http://www.ee.ic.ac.uk/hp/staff/dmb/voicebox/voicebox.html
-%    [y, Fs] = audioread('audio4.wav');
-%    N = length(y);
-%    t = linspace(0, N/Fs, N);
-%    subplot(2,1,1);
-%    plot(t,y);
-%    title('Raw Cough Signal');
-%    xlabel('Time (s)');
-%    ylabel('Amplitude (dB)');
+    [y, Fs] = audioread('audio4.wav');
+    N = length(y);
+    t = linspace(0, N/Fs, N);
+    subplot(2,1,1);
+    plot(t,y);
+    title('Raw Cough Signal');
+    xlabel('Time (s)');
+    ylabel('Amplitude (dB)');
     
-%    subplot(2,1,2);
-%    [c, tc] = melcepst(y, 44100);
-%    plot(tc, c);
-%    title('Mel Frequency Cepstral Coefficients');
-%    xlabel('Frequency (Hz)');
-%    ylabel('Mel Coefficients');    
+    subplot(2,1,2);
+    [c, tc] = melcepst(y, 44100);
+    plot(tc, c);
+    title('Mel Frequency Cepstral Coefficients');
+    xlabel('Frequency (Hz)');
+    ylabel('Mel Coefficients');    
 
     
 %% Server
@@ -28,26 +28,32 @@
     fprintf('Client Connected! \n');
     fprintf('Waiting for Data...\n');
 
-messages = 0;
 timer = 0;
-
-% Allows MATLAB to utilize UTF8 format from now on. 
-feature('DefaultCharacterSet', 'UTF8')
-
+newt = t;
+message = [];
 while t.status == 'open'
+    if newt ~= t
+        fprintf('SOMETHING CHANGED I DONT LIKE CHANGE\n');
+    end
     if t.BytesAvailable > 0
-        message = fread(t, hex2dec(t.BytesAvailable), '2*UTF-16', 2);
-        break
+        % Brice was here, if this doesn't work it's his fault
+        fprintf('THE PROGRAM GOT TO THIS POINT\n')
+        % message = fread(t, t.BytesAvailable);
+        message = [message ; fread(t, t.BytesAvailable)];
+        continue
     else
         fprintf('Waiting for Message...\n');
         pause(0.5)
         timer = timer + 1;
-        if timer == 1
+        if timer == 20
             break
         end
     end
+    newt = t;
 end
-
+if timer == 20
+    fprintf('Timed out, buster.\n');
+end
 % data = fread(t, t.BytesAvailable);
 % plot(data);
 
@@ -57,9 +63,10 @@ end
 % fwrite(t, bye);
 
 fprintf('Message Received!\n');
-fprintf('%s \n', message);
+% please god no, not now    -Brice
+% fprintf('%s \n', message);
 
 fclose(t);
 fprintf('Connection Ended. \n');
-delete(t); 
-clear t 
+% delete(t); 
+% clear t 
